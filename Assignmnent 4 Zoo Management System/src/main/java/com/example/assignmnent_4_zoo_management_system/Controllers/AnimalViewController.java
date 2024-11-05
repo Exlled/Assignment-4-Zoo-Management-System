1c6afbf834d265235a55c665daad9f920dd13df6
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2024-11-05 12:53:23 -0500

    Created lionview

---
+++ Assignmnent 4 Zoo Management System/src/main/java/com/example/assignmnent_4_zoo_management_system/Controllers/AnimalViewController.java
@@ -0,0 +1,45 @@
+package com.example.assignmnent_4_zoo_management_system.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+public class AnimalViewController {
+    @FXML
+    Button addButton;
+    @FXML
+    Button deleteButton;
+    @FXML
+    Button editButton;
+    @FXML
+    Button saveButton;
+    @FXML
+    Button backButton;
+    @FXML
+    Label titleLabel;
+    @FXML
+    ListView<String> listViewForAnimals;
+
+    //actions for back button
+    @FXML
+    protected void onBackButtonClick() {
+
+    }
+    @FXML
+    protected void onAddButtonClick() {
+
+    }
+    @FXML
+    protected void onDeleteButtonClick() {
+
+    }
+    @FXML
+    protected void onEditButtonClick() {
+
+    }
+    @FXML
+    protected void onSaveButtonClick() {
+
+    }
+}
--- Assignmnent 4 Zoo Management System/src/main/java/com/example/assignmnent_4_zoo_management_system/Model/CompositeAnimalCollection.java
+++ Assignmnent 4 Zoo Management System/src/main/java/com/example/assignmnent_4_zoo_management_system/Model/CompositeAnimalCollection.java
@@ -2,6 +2,7 @@ package com.example.assignmnent_4_zoo_management_system.Model;
 
 public class CompositeAnimalCollection {
     public void addCollection(Enclosure enclosure) {
+
     }
 
     public void setName(String bigCats) {
