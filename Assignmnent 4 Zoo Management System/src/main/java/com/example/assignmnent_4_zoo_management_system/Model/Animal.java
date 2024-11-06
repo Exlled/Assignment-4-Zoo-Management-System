e55f1430812ebaa8560aa8ce673ffda4a664e07a
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2024-11-06 09:07:56 -0500

    Using ImportHelper,

--- Assignmnent 4 Zoo Management System/src/main/java/com/example/assignmnent_4_zoo_management_system/Model/Animal.java
+++ Assignmnent 4 Zoo Management System/src/main/java/com/example/assignmnent_4_zoo_management_system/Model/Animal.java
@@ -1,4 +1,31 @@
 package com.example.assignmnent_4_zoo_management_system.Model;
 
-public class Animal {
+public abstract class Animal {
+    private final String aName;  // Attribute
+    private final int aAge;     // Attribute
+    private final int aWeight;  // Attribute (in kg)
+    private final int aHeight;  // Attribute (in cm)
+
+    public Animal(String pName, int pAge, int pWeight, int pHeight) { // Parameters
+        this.aName = pName;
+        this.aAge = pAge;
+        this.aWeight = pWeight;
+        this.aHeight = pHeight;
+    }
+
+    public String getName() {
+        return aName;
+    }
+
+    public int getAge() {
+        return aAge;
+    }
+
+    public int getWeight() {
+        return aWeight;
+    }
+
+    public int getHeight() {
+        return aHeight;
+    }
 }
--- Assignmnent 4 Zoo Management System/src/main/java/com/example/assignmnent_4_zoo_management_system/Model/Enclosure.java
+++ Assignmnent 4 Zoo Management System/src/main/java/com/example/assignmnent_4_zoo_management_system/Model/Enclosure.java
@@ -1,4 +1,10 @@
 package com.example.assignmnent_4_zoo_management_system.Model;
 
 public class Enclosure {
+    public void setName(String lions) {
+    }
+
+    public void addAnimal(Lion plion) {
+        //Lion plion = new Lion(String )
+    }
 }
