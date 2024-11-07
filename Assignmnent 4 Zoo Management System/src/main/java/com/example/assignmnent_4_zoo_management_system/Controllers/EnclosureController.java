39cb93d49d3e1b2ce290e0fff3337e0f9431b5fd
s <Samantha.Jones@stu.crc-lennox.qc.ca>
2024-11-07 09:20:01 -0500

    Created the enclosure-view and added the buttons

--- Assignmnent 4 Zoo Management System/src/main/java/com/example/assignmnent_4_zoo_management_system/Controllers/EnclosureController.java
+++ Assignmnent 4 Zoo Management System/src/main/java/com/example/assignmnent_4_zoo_management_system/Controllers/EnclosureController.java
@@ -1,4 +1,25 @@
 package com.example.assignmnent_4_zoo_management_system.Controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+
 public class EnclosureController {
+    @FXML
+    Label enclosureTItleLabel;
+    @FXML
+    ListView<String> enclosureListView;
+    @FXML
+    Button addbutton;
+    @FXML
+    Button saveButton;
+    @FXML
+    Button deleteButton;
+
+    @FXML
+    protected void initialize() {
+      //  enclosureListView.setItems();
+    }
 }
